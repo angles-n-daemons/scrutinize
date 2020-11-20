3052428e25a51a4b9c2e76d7cf181c83c45f1928
Brian Dillmann <dillmann.brian@gmail.com>
2020-11-19 23:12:31 -0500

    add the migrador

---
+++ docker/migrate_db.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+HOST=${SCRUTINIZE_DB_HOST:-0.0.0.0}
+PORT=${SCRUTINIZE_DB_PORT:-5432}
+DB=${SCRUTINIZE_DB_NAME:-scrutinize}
+USER=${SCRUTINIZE_DB_USER:-postgres}
+PASSWORD=${SCRUTINIZE_DB_PASSWORD:-password}
+
+while ! nc -z ${HOST} ${PORT}; do
+  echo 'Postgres is unavailable.'
+  sleep 1
+done
+
+goose -dir db postgres "host=${HOST} user=${USER} dbname=${DB} password=${PASSWORD} sslmode=disable" up
