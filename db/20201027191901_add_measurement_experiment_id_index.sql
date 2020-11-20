cf46cc517bb741776a76fc19b41d5a178e2c89f6
Brian Dillmann <dillmann.brian@gmail.com>
2020-11-20 11:39:53 -0500

    create proper measurement index

--- db/20201027191901_add_measurement_experiment_id_index.sql
+++ db/20201027191901_add_measurement_experiment_id_index.sql
@@ -1,9 +1,9 @@
 -- +goose Up
 -- +goose StatementBegin
-CREATE INDEX idx_measurement_experiment_id ON Measurement(experiment_id);
+CREATE INDEX idx_measurement_experiment_id_metric ON Measurement(experiment_id, metric_name);
 -- +goose StatementEnd
 
 -- +goose Down
 -- +goose StatementBegin
-DROP INDEX idx_measurement_experiment_id;
+DROP INDEX idx_measurement_experiment_id_metric;
 -- +goose StatementEnd
