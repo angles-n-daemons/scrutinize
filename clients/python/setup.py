8fd787e16a38f477dfa986dce973f3b8100c1516
Brian Dillmann <dillmann.brian@gmail.com>
2020-11-23 14:47:53 -0500

    add long description to pypi

--- clients/python/setup.py
+++ clients/python/setup.py
@@ -1,8 +1,11 @@
 from setuptools import setup
 
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
 setup(
     name='scrutinize',
-    version='0.1',
+    version='0.1.1',
     description='the lightweight experimentation platform',
     url='http://github.com/angles-n-daemons/scrutinize',
     author='brian dillmann',
