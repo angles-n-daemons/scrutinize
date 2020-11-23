a558e6ab3be883cd4614e47fbc0ee69181728db2
Brian Dillmann <dillmann.brian@gmail.com>
2020-11-23 14:50:54 -0500

    earlier version

--- clients/python/setup.py
+++ clients/python/setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setup(
     name='scrutinize',
-    version='0.1.2',
+    version='0.0.1',
     description='the lightweight experimentation platform',
     long_description=long_description,
     long_description_content_type="text/markdown",
